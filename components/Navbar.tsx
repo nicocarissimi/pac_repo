b5e882e139ec4bab4a15690291a3dc62b76919d1
nicocarissimi <you@example.com>
2024-08-21 00:55:26 +0200

    Added link to home button in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,9 +54,9 @@ const Navbar = ({onSearchChange}:{onSearchChange: (value: string)=> void }) => {
   return (
     <nav className="w-full fixed z-40">
       <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
-        <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
+        <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo"/>
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          <NavbarItem label="Home" active />
+        <div onClick={()=>router.push('/')}><NavbarItem label="Home" active /></div>
           <div onClick={()=>router.push('/playlist')}><NavbarItem label="Playlist" /></div>
           <NavbarItem label="My List" />
         </div>
