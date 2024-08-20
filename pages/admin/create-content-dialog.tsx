bea1688a809bfd59fca1a73a480e609bacd2e429
jbommarito <jonathan.bommarito@sorint.com>
2024-08-21 00:59:34 +0200

    created create-component-dialog to create new item in admin page

---
+++ pages/admin/create-content-dialog.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { PlusIcon } from "@heroicons/react/24/solid"
+
+export function CreateContentDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="mr-12 rounded-2xl">
+            <PlusIcon className="w-6 h-6" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]" onInteractOutside={(e)=>{e.preventDefault()}}>
+        <DialogHeader>
+          <DialogTitle>Create new content</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="title" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="title"
+              defaultValue="New Content"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-right">
+              Description
+            </Label>
+            <Input
+              id="description"
+              defaultValue="new description"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+            <Button variant={"outline"}>Close</Button>
+            <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
