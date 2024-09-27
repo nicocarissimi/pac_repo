4a3bd62781b60a868b6428627df9fb7ed1a083c5
jonnybommarito <j.bommarito@studenti.unibg.it>
2024-09-27 19:41:57 +0200

    add dropdown menu in playlist items list

--- components/PlaylistDisplay.tsx
+++ components/PlaylistDisplay.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { PlaylistInterface } from '@/types';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { CreditCard, Ellipsis, Keyboard, Plus, Settings, User, Users } from 'lucide-react';
+import { ScrollArea } from './ui/scroll-area';
+import { Separator } from './ui/separator';
 
 interface PlaylistDisplayProps {
   playlists: PlaylistInterface[];
@@ -8,32 +12,57 @@ interface PlaylistDisplayProps {
 
 const PlaylistDisplay: React.FC<PlaylistDisplayProps> = ({ playlists, myPlaylists }) => {
   return (
-    <div className="flex flex-col gap-2">
-      {playlists.map((playlist) => (
-        <div
-          key={playlist.id}
-          className="flex flex-row gap-2 rounded-2xl bg-zinc-800 text-white p-2 relative"
-        >
-          <span>{playlist.name}</span>
-          {myPlaylists && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="absolute right-3 top-2 w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
-          )}
-        </div>
-      ))}
-    </div>
+    <ScrollArea className="rounded-md w-full h-full">
+      <div className="p-4">
+        { playlists.map((p) => (
+          <div>
+            <div key={p.id} className="text-md text-white flex w-full justify-between">
+              <p>{p.name}</p>
+              <DropdownMenu modal={false}>
+              <DropdownMenuTrigger>
+                  <Ellipsis size={"24px"} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align='end'>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard className="mr-2 h-4 w-4" />
+                    <span>Billing</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Keyboard className="mr-2 h-4 w-4" />
+                    <span>Keyboard shortcuts</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Users className="mr-2 h-4 w-4" />
+                    <span>Team</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Plus className="mr-2 h-4 w-4" />
+                    <span>New Team</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+              </DropdownMenuContent>
+            </DropdownMenu>
+            </div>
+            <Separator className='my-2'/>
+          </div>
+       ))}
+      </div>
+    </ScrollArea>
   );
 };
 
--- pages/playlist.tsx
+++ pages/playlist.tsx
@@ -43,7 +43,6 @@ const PlaylistPage = () => {
   const [isPublic, setIsPublic] = useState(true);
   const [myPlaylists, setMyPlaylists] = useState(true)
   const [showModal, setShowModal] = useState(false);
-  const { data: movies = [] } = useMovieList();
 
   const handleAddPlaylist = () => {
     setShowModal(true);
@@ -79,29 +78,22 @@ const PlaylistPage = () => {
     console.log(value)
   }
 
-  const playlists: PlaylistInterface[] = [
-    {id: "1", name:"primo nome a caso", userId:"123", isPublic: false},
-    {id: "2", name:"secondo nome a caso", userId:"123", isPublic: false},
-    {id: "3", name:"terzo nome a caso", userId:"123", isPublic: false},
-    {id: "4", name:"quarto nome a caso", userId:"123", isPublic: false},
-  ]
-
   return (
     <RootLayout onChangeValue={handleChangeSearchValue}>
-      <div className='flex w-full h-[93%] mt-20 absolute justify-around gap-2 p-10'>
+      <div className='flex w-full h-[90%] mt-20 absolute justify-around gap-2 p-2'>
           <div className='flex flex-col h-full justify-between w-[40%]'>
             <div className='rounded-xl p-8 bg-zinc-900 h-full '>
             <div className='flex relative mb-5 text-white flex-row'>
             <div className='flex me-auto ml-2'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="size-6">
+              <path d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
             </svg>
             <span className='ml-5'>Your Library</span> 
             </div>
             
             <div className='block mr-8'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='h-full hover:rounded-full hover:bg-zinc-800' onClick={handleAddPlaylist}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className='h-full hover:rounded-full hover:bg-zinc-800' onClick={handleAddPlaylist}>
+            <path d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
             </div>
             </div>
@@ -119,8 +111,8 @@ const PlaylistPage = () => {
                 className="bg-zinc-800 rounded-2xl p-2 pl-10 text-gray-500 placeholder:text-gray-500 w-full"
                 placeholder="search your father"
               />
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="absolute left-3 top-2 w-6 h-6 text-gray-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="absolute left-3 top-2 w-6 h-6 text-gray-500">
+              <path d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
             </div>
             <ScrollArea className=' mt-5 h-[90%]'>
@@ -139,8 +131,8 @@ const PlaylistPage = () => {
                 className="bg-zinc-800 rounded-2xl p-2 pl-10 text-gray-500 placeholder:text-gray-500 w-full"
                 placeholder="search your father"
               />
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="absolute left-3 top-2 w-6 h-6 text-gray-500">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="absolute left-3 top-2 w-6 h-6 text-gray-500">
+              <path d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
             </div>
           
