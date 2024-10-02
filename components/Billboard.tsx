73e39ae6111ac1646c99005a97fbf205b8b6d29c
jonnybommarito <j.bommarito@studenti.unibg.it>
2024-10-02 16:55:13 +0200

    fixed billboard dimensions

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -16,10 +16,10 @@ const Billboard: React.FC = () => {
 
 
   return (
-    <div className="relative h-[56.25vw]">
-      <video poster={data?.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={data?.videoUrl}></video>
-      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
-        <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+    <div className="relative h-[35vw]">
+      <video poster={data?.thumbnailUrl} className="w-full h-[35vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={data?.videoUrl}></video>
+      <div className="absolute top-[30%] md:top-[20%] ml-4 md:ml-16">
+        <p className="text-white text-1xl md:text-5xl h-full w-[60%] lg:text-6xl font-bold drop-shadow-xl">
           {data?.title}
         </p>
         <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
