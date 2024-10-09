cfe05c446fb5aca48ea7cf0280878d2b4ff398fb
jonnybommarito <j.bommarito@studenti.unibg.it>
2024-10-09 18:21:23 +0200

    changed next.config.js to fetch thumbnailUrl from all domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',  // Consente tutti gli host
+      },
+    ],
+  },
+};
