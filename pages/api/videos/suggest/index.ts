5d0595b4c046c1b771789462bb5f2870d33ec7d8
jonnybommarito <j.bommarito@studenti.unibg.it>
2024-10-26 14:27:49 +0200

    greedy algorithm: splitted fetching from db and evaluate solution

--- pages/api/videos/suggest/index.ts
+++ pages/api/videos/suggest/index.ts
@@ -1,10 +1,9 @@
-import { CategoryInterface } from "@/libs/definitions";
+import { CategoryInterface, VideoInterface } from "@/libs/definitions";
 import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function GET(learning_time: number, favoriteCategories: string[] ){
         // candidates i a list of combinations so a list of list
-        const solution = []
         // if duration > learning_time automatically video can't be inside a valid candidate
         // where candidate is a combination of video where the sum all video durations is less than or equals to learning_time value
         const result = await prismadb.video.findMany({
@@ -48,33 +47,52 @@ async function GET(learning_time: number, favoriteCategories: string[] ){
             }
         });
 
-        
         var videos = result.map(video => ({
             ...video,
             categories: video.categories.map(cat => cat.category.name)
         }));
-
-        var total = 0
-        if(videos.length > 0) {
-            videos.sort((a,b) => a.categories.length - b.categories.length)[videos.length]
-            const video = videos.pop()
-            total += video!.duration
-            solution.push(video)
-        }
-
-
-        while( videos.length > 0 && total < learning_time) {
-            // sort all candidates and choose the first one which has the most categories number
-            // once i selected the first candidates the algorithm must select the best candidates looking for the amount of category not already included in the solution list
-            const selectedVideo = candidateSelection(solution, videos)
-            if (selectedVideo.duration + total < learning_time) {
-                solution.push(selectedVideo)
-                total += selectedVideo.duration
-            }
-            videos = videos.filter(video => video !== selectedVideo)
-        }
+        
+        const solution = evaluateSolution(videos, learning_time)
 
         return solution
+
+}
+
+interface Video {
+    categories: string[];
+    id: string;
+    title: string;
+    description: string;
+    videoUrl: string;
+    thumbnailUrl: string;
+    duration: number;
+}
+
+
+function evaluateSolution(videos: Video[], learning_time: number){
+    const solution = []
+
+    var total = 0
+    if(videos.length > 0) {
+        videos.sort((a,b) => a.categories.length - b.categories.length)[videos.length]
+        const video = videos.pop()
+        total += video!.duration
+        solution.push(video)
+    }
+
+
+    while( videos.length > 0 && total < learning_time) {
+        // sort all candidates and choose the first one which has the most categories number
+        // once i selected the first candidates the algorithm must select the best candidates looking for the amount of category not already included in the solution list
+        const selectedVideo = candidateSelection(solution, videos)
+        if (selectedVideo.duration + total < learning_time) {
+            solution.push(selectedVideo)
+            total += selectedVideo.duration
+        }
+        videos = videos.filter(video => video !== selectedVideo)
+    }
+
+    return solution
 
 }
 
@@ -99,8 +117,6 @@ function candidateSelection(solution: any[], videos:  any[]){
     return maxVideo
 }
 
-
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const {currentUser} = await serverAuth(req)
@@ -108,7 +124,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if(currentUser.learning_time && currentUser.learning_time > 0) {
             const {favoriteCategories, learning_time} = currentUser
             const categories = favoriteCategories.map(c=>c.category.name)
-            const videos = await GET(15, categories)
+            const videos = await GET(learning_time, categories)
             return res.status(200).json(videos)    
         }
         return res.status(200).json({})
